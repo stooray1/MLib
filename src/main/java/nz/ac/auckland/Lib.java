00d766013dcef97059d8eb9313b4f340170e4f2f
Tariq <t.khan@auckland.ac.nz>
2018-10-09 15:37:55 +1300

    Mathod added to find smaller of three int values

--- src/main/java/nz/ac/auckland/Lib.java
+++ src/main/java/nz/ac/auckland/Lib.java
@@ -19,5 +19,23 @@ public class Lib {
 			m=c;
 		return m;
 	}
+	
+	/**
+	 * Returns the smaller of three int values.
+	 * If the arguments have the same value, the result is that same value.
+	 * 
+	 * @param a first argument 
+	 * @param b second argument
+	 * @param b third argument  
+	 * @return the smaller of a, b and c
+	 */
+	public static int min(int a, int b, int c) {
+		int m=a
+		if(m<b)
+			m=b;
+		if(m<c)
+			m=c;
+		return m;
+	}
 
 }
