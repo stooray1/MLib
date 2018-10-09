6ffd4c9b4443f8380b97631f34044b9a0de71c00
Tariq <t.khan@auckland.ac.nz>
2018-10-09 15:01:05 +1300

    This class for static mathematical methods > max method added

---
+++ src/nz/ac/auckland/Lib.java
@@ -0,0 +1,23 @@
+package nz.ac.auckland;
+
+public class Lib {
+	
+	/**
+	 * Returns the greater of three int values.
+	 * If the arguments have the same value, the result is that same value.
+	 * 
+	 * @param a first argument 
+	 * @param b second argument
+	 * @param b third argument  
+	 * @return the larger of a, b and c
+	 */
+	public static int max(int a, int b, int c) {
+		int m=a;
+		if(m<b)
+			m=b;
+		if(m<c)
+			m=c;
+		return m;
+	}
+
+}
